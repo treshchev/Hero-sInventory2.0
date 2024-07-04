9bef4d65d52267d18aa9fd5ff84792f86a55c11b
Vladislav Treshchev <letsgovlad@gmail.com>
2024-07-04 13:03:58 +0300

    Created some code

--- Hero's inventory 2.0/Hero's inventory 2.0.cpp
+++ Hero's inventory 2.0/Hero's inventory 2.0.cpp
@@ -1,8 +1,54 @@
+// Program Hero's inventory 2.0
+// Describes work with vectors
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	std::vector<std::string> Inventory;
+	Inventory.push_back("sword");
+	Inventory.push_back("armor");
+	Inventory.push_back("shield");
+	std::cout << "You have " << Inventory.size() << " items.\n";
+	std::cout << "\nYour items: \n";
 
+	for (unsigned int i = 0; i < Inventory.size(); i++)
+	{
+		std::cout << Inventory[i] << std::endl;
+	}
+
+	std::cout << "\nYou trade your sword for a battle axe.";
+	Inventory[0] = "battle axe";
+	std::cout << "\nYour items: \n";
+
+	for (unsigned int i = 0; i < Inventory.size(); i++)
+	{
+		std::cout << Inventory[i] << std::endl;
+	}
+
+	std::cout << "\nThe item name '" << Inventory[0] << "' has";
+	std::cout << Inventory[0].size() << " letters in it.\n";
+	std::cout << "\nYour shield is destroyed in a fierce battle.";
+	Inventory.pop_back();
+	std::cout << "Your items:\n";
+
+	for (unsigned int i = 0; i < Inventory.size(); i++)
+	{
+		std::cout << Inventory[i] << std::endl;
+	}
+
+	std::cout << "\n You were robbed of all of your possessions by a thief.";
+	Inventory.clear();
+	if (Inventory.empty())
+	{
+		std::cout << "\nYou lost all your items.\n";
+	}
+	else
+	{
+		std::cout << "\nYou have at least one item.\n";
+	}
+
+	return 0;
+}
